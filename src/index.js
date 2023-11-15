ef3775b951de78b5f458dada49e8e87ca8ad776d
manu <manuel.hernandez@ing.austral.edu.ar>
2023-11-15 09:13:24 -0300

    enabled cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const { exec } = require("child_process");
 const { PrismaClient } = require('@prisma/client');
 const broker = process.env.BROKER_SERVER
@@ -6,6 +7,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 const db = new PrismaClient();
 
 const getMessage = (channel, message) => {
